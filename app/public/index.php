9059301d7fde91df387e02c03f5c8dc1c571e881
suki <88000sofien@gmail.com>
2026-01-03 17:03:47 +0100

    ajout commentaire utilisation api geoloc ip

--- app/public/index.php
+++ app/public/index.php
@@ -1,5 +1,8 @@
 <?php
 
+// api GEOLOC IP
+// https://ipapi.co/{ip}/{format}/
+
 $url_api_meteo = "https://www.infoclimat.fr/public-api/gfs/xml?_ll=48.67103,6.15083&_auth=ARsDFFIsBCZRfFtsD3lSe1Q8ADUPeVRzBHgFZgtuAH1UMQNgUTNcPlU5VClSfVZkUn8AYVxmVW0Eb1I2WylSLgFgA25SNwRuUT1bPw83UnlUeAB9DzFUcwR4BWMLYwBhVCkDb1EzXCBVOFQoUmNWZlJnAH9cfFVsBGRSPVs1UjEBZwNkUjIEYVE6WyYPIFJjVGUAZg9mVD4EbwVhCzMAMFQzA2JRMlw5VThUKFJiVmtSZQBpXGtVbwRlUjVbKVIuARsDFFIsBCZRfFtsD3lSe1QyAD4PZA%3D%3D&_c=19f3aa7d766b6ba91191c8be71dd1ab2";
 $meteo_content = file_get_contents($url_api_meteo);
 
